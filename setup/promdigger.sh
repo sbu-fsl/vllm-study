d67828308e0f4c0c7ef8e1bfc571fffd01e84f8c
amirhnajafiz <najafizadeh21@gmail.com>
2026-02-03 16:51:15 -0500

    add: archive metrics

--- setup/promdigger.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# clone in the prometheus-digger repository
-git clone https://github.com/amirhnajafiz/prometheus-digger.git
-cd prometheus-digger
-
-# call install
-sudo ./scripts/install.sh
