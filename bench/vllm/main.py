5365dffd2c8c4a594b73ef1a49c1d80321e92c80
amirhnajafiz <najafizadeh21@gmail.com>
2026-02-09 13:08:30 -0500

    update: main of bench

--- bench/vllm/main.py
+++ bench/vllm/main.py
@@ -1,8 +1,32 @@
+import requests
+
 from benchmarks.mmlu import MMLUBenchmark
+
+
+API_BASE = "http://localhost:8000/v1"
+REQUEST_PERIOD = 30  # seconds
 
 
 if __name__ == "__main__":
     benchmark = MMLUBenchmark.main()
 
     for result in benchmark.run():
-        print(result)
+        url = f"{API_BASE}{result["uri"]}"
+        headers = {
+            "Content-Type": "application/json",
+        }
+
+        try:
+            response = requests.post(
+                url,
+                headers=headers,
+                json=result["payload"],
+                timeout=REQUEST_PERIOD,
+            )
+            response.raise_for_status()
+            print(response.json()["choices"][0]["message"]["content"].strip())
+
+        except requests.exceptions.Timeout:
+            print("⏱️ Request timed out")
+        except Exception as e:
+            print(f"❌ Request failed: {e}")
