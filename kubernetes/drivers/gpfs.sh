23b9855fb5303588249968e6373f1948783a2ca0
amirhnajafiz <najafizadeh21@gmail.com>
2026-02-23 15:11:12 -0500

    fix: storage init

--- kubernetes/drivers/gpfs.sh
+++ kubernetes/drivers/gpfs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+NFS_IP=$1
+
 helm upgrade --install gpfs-nfs-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
-  --set nfs.server=130.245.176.57 \
+  --set nfs.server=$NFS_IP \
   --set nfs.path=/gpfs/fs1/suny-ibm \
   --set storageClass.name=gpfs-nfs \
   --set storageClass.defaultClass=false \
--- kubernetes/drivers/nfs.sh
+++ kubernetes/drivers/nfs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+NFS_IP=$1
+
 helm install nfs-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
-  --set nfs.server=130.245.126.6 \
+  --set nfs.server=$NFS_IP \
   --set nfs.path=/workloads/sunyibm/data \
   --set storageClass.defaultClass=true \
   --set storageClass.reclaimPolicy=Delete \
